890772a9da186a28cdd077623c81cb8235967c61
sherlock wisdom <wisdomnji@gmail.com>
2019-08-20 05:04:36 +0100

    added epoch timestamp to it, interesting fact epoch began 00:00:00 jan 01 1970

--- scripts/modem_information_extraction.sh
+++ scripts/modem_information_extraction.sh
@@ -3,17 +3,17 @@
 
 #check for available modems
 if [ "$1" == "list" ] ; then
-	mmcli -L | grep -oe "[/\s][0-9]." | cut -b 2-
+	mmcli -L | grep -oP "[/\s][0-9]." | cut -b 2-
 
 elif [ "$1" == "extract" ] ; then
 	modem_index=$3
 	_command=$2
 	if [ "$_command" == "e_id" ] ; then
-		mmcli -m $modem_index 2>&1 | grep "equipment id" | grep -oe "[0-9]*"
+		mmcli -m $modem_index 2>&1 | grep "equipment id" | grep -oP "[0-9]*"
 	elif [ "$_command" == "sig_qual" ] ; then
-		mmcli -m $modem_index 2>&1 | grep "signal quality" | grep -oe "[0-9]*"
+		mmcli -m $modem_index 2>&1 | grep "signal quality" | grep -oP "[0-9]*"
 	elif [ "$_command" == "op_name" ] ; then
-		mmcli -m 8 | grep "operator name" | grep -oe ": [a-zA-Z]*" | grep -oe "[a-zA-Z]*"
+		mmcli -m 8 | grep "operator name" | grep -oP ": [a-zA-Z]*" | grep -oP "[a-zA-Z]*"
 	fi
 
 elif [ "$1" == "sms" ] ; then
@@ -24,7 +24,7 @@ elif [ "$1" == "sms" ] ; then
 		modem_index=$5
 		
 		output=$( mmcli -m $modem_index --messaging-create-sms="text='$message', number='$number'" )
-		sms_index=$( echo $output | grep -oe "[/\s][0-9]." | cut -b 2- )
+		sms_index=$( echo $output | grep -oP "[/\s][0-9]." | cut -b 2- )
 		if [ $(mmcli -m $modem_index -s $sms_index --send > /dev/null 2>&1) ] ; then
 			echo sent
 		else
@@ -33,7 +33,7 @@ elif [ "$1" == "sms" ] ; then
 	elif [ "$_type" == "received" ] ; then
 		modem_index=$3
 
-		output=$( mmcli -m $modem_index --messaging-list-sms | grep -oe "[0-9]* (received)" | grep -oe "[0-9]*" )
+		output=$( mmcli -m $modem_index --messaging-list-sms | grep -oP "[0-9]* (received)" | grep -oP "[0-9]*" )
 		if [ ! -z "$output" ] ; then 
 			echo "$output"
 		fi
@@ -41,9 +41,10 @@ elif [ "$1" == "sms" ] ; then
 		modem_index=$4
 		message_index=$3
 
-		message_number=$( mmcli -m $modem_index --sms $message_index| grep number | grep -oe "[+0-9]*" )
-		message_text=$( mmcli -m $modem_index --sms $message_index | grep text | grep -oe ": [a-zA-Z0-9\W :<=?]*" | cut -b 3- )
-		echo -e "$message_number\n$message_text"
+		message_number=$( mmcli -m $modem_index --sms $message_index| grep number | grep -oP "[+0-9]*" )
+		message_text=$( mmcli -m $modem_index --sms $message_index | grep text | grep -oP ": [a-zA-Z0-9\W :<=?]*" | cut -b 3- )
+		timestamp=$( mmcli -m $modem_index --sms $message_index | grep timestamp: | grep -oP ": [a-zA-Z0-9\W]*" | cut -b 3- ) 
+		echo -e "$message_number\n$message_text\n$timestamp"
 
 	fi
 fi
